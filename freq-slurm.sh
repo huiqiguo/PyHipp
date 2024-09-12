6280d598fbaf0d2ec0f65dfa7b55297be1dbe09e
AWS ParallelCluster user <ec2-user@ip-10-0-1-86.ap-southeast-1.compute.internal>
2024-09-12 17:21:14 +0000

    edited freq-slurm and fsall-slurm

--- freq-slurm.sh
+++ freq-slurm.sh
@@ -15,4 +15,4 @@
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
 pyh.FreqSpectrum(saveLevel=1); \
-pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);
+pyh.FreqSpectrum(loadHighPass=True, pointsPerWindow=3000, saveLevel=1);"
